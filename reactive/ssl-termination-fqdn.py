d1dbe44cfd0a700cdb46c5cb143be2aea6908595
sebastienpattyn93 <sebastien.pattyn@gmail.com>
2019-01-23 16:38:23 +0100

    update to use as k8s fqdn charm

--- reactive/ssl-termination-fqdn.py
+++ reactive/ssl-termination-fqdn.py
@@ -14,9 +14,10 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
-from charmhelpers.core import unitdata
-from charmhelpers.core.hookenv import status_set, config
+
+import yaml
+from charmhelpers.core.templating import render
+from charmhelpers.core.hookenv import status_set, config, application_name
 from charms.reactive import when, when_not, set_flag, clear_flag, when_any
 from charms.reactive.relations import endpoint_from_flag
 
@@ -29,46 +30,65 @@ config = config()
 ########################################################################
 
 @when('endpoint.ssl-termination.available')
-@when_not('website.available')
+@when_not('endpoint.kubernetes-deployer.available')
 def missing_http_relation():
     clear_flag('client.cert-created')
-    status_set('blocked', 'Waiting for http relation')
+    status_set('blocked', 'Waiting for kubernetes-deployer relation')
 
 
-@when('website.available')
+@when('endpoint.kubernetes-deployer.available')
 @when_not('endpoint.ssl-termination.available')
 def missing_ssl_termination_relation():
     status_set('blocked', 'Waiting for ssl-termination-proxy relation')
 
 
 @when_any('config.changed.fqdns',
-          'config.changed.credentials')
+          'config.changed.credentials',
+          'config.changed.nodeport',
+          'endpoint.kubernetes-deployer.changed')
 def fqdns_changed():
     clear_flag('client.cert-requested')
     clear_flag('client.cert-created')
 
 
 ########################################################################
-# Configure certificate
+# Configure certificateS
 ########################################################################
 
-@when('website.available',
+@when('endpoint.kubernetes-deployer.available',
       'endpoint.ssl-termination.available')
-@when_not('client.cert-requested')
-def create_cert_request():
+@when_not('client.cert-requested', 'client.k8s-requested')
+def send_k8s_request():
     if not config.get('fqdns'):
         status_set('blocked', 'Waiting for fqdns config')
         return
-    ssl_termination = endpoint_from_flag('endpoint.ssl-termination.available')
-    website = endpoint_from_flag('website.available')
+    if not config.get('nodeport'):
+        status_set('blocked', 'Waiting for nodeport config')
+        return
+    endpoint = endpoint_from_flag('endpoint.kubernetes-deployer.available')
+    context = {'name': application_name(), 'fqdns': config.get('fqdns'), 'nodeport': config.get('nodeport')}
+    resource = render('resource.yaml', None, context)
+    endpoint.send_create_request([yaml.load(resource)])
+    set_flag('client.k8s-requested')
 
-    services = website.services()
-    if not services:
+
+@when('endpoint.kubernetes-deployer.new-status',
+      'endpoint.ssl-termination.available',
+      'client.k8s-requested')
+@when_not('client.cert-requested')
+def create_cert_request():
+    endpoint = endpoint_from_flag('endpoint.kubernetes-deployer.new-status')
+    ssl_termination = endpoint_from_flag('endpoint.ssl-termination.available')
+    workers = endpoint.get_worker_ips()
+    if not workers:
         return
 
     upstreams = []
-    for service in services:
-        upstreams.extend(service['hosts'])
+    for worker in workers:
+        host = [{'hostname': worker,
+                'private-address': worker,
+                'port': config.get('nodeport')}]
+        upstreams.extend(host)
 
     ssl_termination.send_cert_info({
         'fqdn': config.get('fqdns').rstrip().split(),
@@ -80,8 +100,9 @@ def create_cert_request():
     set_flag('client.cert-requested')
 
 
-@when('website.available',
-      'endpoint.ssl-termination.update')
+@when('endpoint.kubernetes-deployer.available',
+      'endpoint.ssl-termination.update',
+      'client.cert-requested')
 @when_not('client.cert-created')
 def check_cert_created():
     ssl_termination = endpoint_from_flag('endpoint.ssl-termination.update')
@@ -104,7 +125,7 @@ def check_cert_created():
 
 @when('endpoint.ssl-termination.available',
       'client.cert-requested')
-@when_not('website.available')
+@when_not('endpoint.kubernetes-deployer.available')
 def website_removed():
     endpoint = endpoint_from_flag('endpoint.ssl-termination.available')
     endpoint.send_cert_info({})
